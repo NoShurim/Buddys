88435ab3dc51be5cbc47941df46e601500474559
NoShurim <Cair139Cair139@gmail.com>
2017-07-17 05:19:04 -0300

    Teste 3

--- Kayn_BETA_Fixed/Kayn_BETA_Fixed/Menus.cs
+++ Kayn_BETA_Fixed/Kayn_BETA_Fixed/Menus.cs
@@ -6,7 +6,7 @@ namespace Kayn_BETA_Fixed
 {
     class Menus
     {
-        public static Menu Kmenu, Combo, Lane, Jungle, Misc;
+        public static Menu Kmenu, Combo, Lane, Jungle, Draws, Misc;
 
         internal static void CreateMenu()
         {
@@ -19,11 +19,22 @@ namespace Kayn_BETA_Fixed
             Lane = Kmenu.AddSubMenu("Lane");
             Lane.Add("Qlane", new CheckBox("Use [Q]"));
             Lane.Add("WLane", new CheckBox("Use [W]"));
+            Lane.Add("qmana", new Slider("Mini Mana [Q]", 65, 1));
+            Lane.AddLabel("Mana W");
+            Lane.Add("wmana", new Slider("Mini Mana [W]", 65, 1));
             Jungle = Kmenu.AddSubMenu("Jungle");
             Jungle.Add("Qjungle", new CheckBox("Use [Q]"));
             Jungle.Add("Wjungle", new CheckBox("Use [W]"));
+            Jungle.Add("qmana", new Slider("Mini Mana [Q]", 65, 1));
+            Jungle.AddLabel("Mana W");
+            Jungle.Add("wmana", new Slider("Mini Mana [W]", 65, 1));
             Misc = Kmenu.AddSubMenu("Misc");
             Misc.Add("KS", new CheckBox("Use [R] KillSteal"));
+            Draws = Kmenu.AddSubMenu("Drawings");
+            Draws.Add("DQ", new CheckBox("Use [Q] Draw"));
+            Draws.Add("DW", new CheckBox("Use [W] Draw"));
+            Draws.Add("DE", new CheckBox("Use [E] Draw", false));
+            Draws.Add("DR", new CheckBox("Use [R] Draw"));
 
         }
     }
--- Kayn_BETA_Fixed/Kayn_BETA_Fixed/Program.cs
+++ Kayn_BETA_Fixed/Kayn_BETA_Fixed/Program.cs
@@ -1,12 +1,10 @@
 ﻿using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Events;
-using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static Kayn_BETA_Fixed.Menus;
 
 namespace Kayn_BETA_Fixed
 {
@@ -32,10 +30,84 @@ namespace Kayn_BETA_Fixed
             {
                 return;
             }
-            Menus.CreateMenu();
+            CreateMenu();
             InitializeSpells();
+            Drawing.OnDraw += OnDraw;
+            Game.OnTick += OnTick;
         }
 
+        private static void OnDraw(EventArgs args)
+        {
+            if (Draws["DQ"].Cast<CheckBox>().CurrentValue && Q.IsReady())
+            {
+                Q.DrawRange(System.Drawing.Color.LightBlue);
+            }
+            if (Draws["DE"].Cast<CheckBox>().CurrentValue && E.IsReady())
+            {
+                E.DrawRange(System.Drawing.Color.Crimson);
+            }
+            if (Draws["DR"].Cast<CheckBox>().CurrentValue && R.IsReady())
+            {
+                R.DrawRange(System.Drawing.Color.LawnGreen);
+            }
+        }
+        private static void OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.Equals(Orbwalker.ActiveModes.Combo))
+            {
+                byCombo();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                ByLane();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+            {
+                ByJungle();
+            }
+        }
+        private static void byCombo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ByLane()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ByJungle()
+        {
+            if (Jungle["Qjungle"].Cast<CheckBox>().CurrentValue && Q.IsReady()) //range missing
+            {
+                var jungleMonsterQ = EntityManager.MinionsAndMonsters.GetJungleMonsters(Player.Instance.Position, Q.Range);
+                if (jungleMonsterQ != null)
+                {
+                    foreach (var junglemonsterq in jungleMonsterQ)
+                    {
+                        if (Player.Instance.ManaPercent >= Jungle["qmana"].Cast<Slider>().CurrentValue)
+                        {
+                            Q.Cast(junglemonsterq);
+                        }
+                    }
+                }
+            }
+
+            if (Jungle["Wjungle"].Cast<CheckBox>().CurrentValue && W.IsReady()) //range missing
+            {
+                var jungleMonsterW = EntityManager.MinionsAndMonsters.GetJungleMonsters(Player.Instance.Position, W.Range);
+                if (jungleMonsterW != null)
+                {
+                    foreach (var junglemonstere in jungleMonsterW)
+                    {
+                        if (Player.Instance.ManaPercent >= Jungle["wmana"].Cast<Slider>().CurrentValue)
+                        {
+                           W.Cast(junglemonstere);
+                        }
+                    }
+                }
+            }
+        }
         private static void InitializeSpells()
         {
             Q = new Spell.Skillshot(SpellSlot.Q, 350, EloBuddy.SDK.Enumerations.SkillShotType.Linear, 150, 75, 37);
