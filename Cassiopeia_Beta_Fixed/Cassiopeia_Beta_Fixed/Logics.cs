2afe6d3d11159d52cf0a159194b290b16eb97522
NoShurim <Cair139Cair139@gmail.com>
2017-08-09 20:33:53 -0300

    Test Mode Update

--- Cassiopeia_Beta_Fixed/Cassiopeia_Beta_Fixed/Logics.cs
+++ Cassiopeia_Beta_Fixed/Cassiopeia_Beta_Fixed/Logics.cs
@@ -1,7 +1,9 @@
 ﻿using EloBuddy;
 using EloBuddy.SDK;
+using static Cassiopeia_Beta_Fixed.SpellManager;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Cassiopeia_Beta_Fixed
 {
@@ -75,5 +77,46 @@ namespace Cassiopeia_Beta_Fixed
         {
             return GetBlazeRemainingDamage(unit) >= unit.Health;
         }
+        public static float GetMinionTarget(this Obj_AI_Base target, SpellSlot slot)
+        {
+            var damageType = DamageType.Magical;
+            var ap = Player.Instance.TotalMagicalDamage;
+            var sLevel = Player.GetSpell(slot).Level - 1;
+
+            var dmg = 0f;
+
+            switch (slot)
+            {
+                case SpellSlot.Q:
+                    if (Q.IsReady())
+                        dmg += new float[] { 75, 120, 165, 210, 255 }[sLevel] + 0.70f * ap;
+                    break;
+                case SpellSlot.W:
+                    if (W.IsReady())
+                        dmg += new float[] { 10, 15, 20, 25, 30 }[sLevel] + 0.10f * ap;
+                    break;
+                case SpellSlot.E:
+                    if (E.IsReady() && !(target.HasBuffOfType(BuffType.Poison)))
+                        dmg += new float[] { 64, 70, 80, 90, 110 }[sLevel] + 0.10f * ap;
+                    if (E.IsReady() && target.HasBuffOfType(BuffType.Poison))
+                        dmg += new float[] { 66, 100, 154, 192, 222 }[sLevel] + 0.55f * ap;
+                    break;                 
+                case SpellSlot.R:
+                    if (R.IsReady())
+                        dmg += new float[] { 150, 250, 350 }[sLevel] + 0.50f * ap;
+                    break;                 
+            }
+
+            return Player.Instance.CalculateDamageOnUnit(target, damageType, dmg - 10);
+        }
+
+        public static float GetRealDamage(this Obj_AI_Base target)
+        {
+            var slots = new[] { SpellSlot.Q, SpellSlot.W, SpellSlot.E, SpellSlot.R };
+            var dmg = Player.Spells.Where(s => slots.Contains(s.Slot)).Sum(s => target.GetMinionTarget(s.Slot));
+
+            return dmg;
+        }
     }
 }
+
--- Cassiopeia_Beta_Fixed/Cassiopeia_Beta_Fixed/Program.cs
+++ Cassiopeia_Beta_Fixed/Cassiopeia_Beta_Fixed/Program.cs
@@ -35,7 +35,6 @@ namespace Cassiopeia_Beta_Fixed
             //Comands
             Interrupter.OnInterruptableSpell += OnInterrupter;
             Gapcloser.OnGapcloser += OnGapcloser;
-            Orbwalker.OnPreAttack += DisableAAreset;
             Drawing.OnDraw += Drawing_OnDraw;
             Game.OnTick += Game_OnTick;
 
@@ -63,224 +62,95 @@ namespace Cassiopeia_Beta_Fixed
 
         private static void ByCombo()
         {
-            const int range = 850;
-            const float aoeratio = 0.2f;
+            var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
 
-            var enemies = EntityManager.Heroes.Enemies.Where(n => n.IsValidTarget(range));
-            var selectedTarget = TargetSelector.GetTarget(range, DamageType.Magical);
-            var allTargets = new[] { selectedTarget }.Concat(enemies.Where(n => n.Index != selectedTarget.Index).OrderByDescending(n => Logics.TotalDamage(SpellSlot.Q, n) / n.Health)).Where(n => n.IsValidTarget() && !n.WillDie());
-
-            if (selectedTarget == null && !enemies.Any())
-            {
+            if ((target == null) || target.IsInvulnerable)
                 return;
+
+            var PositionEnemys = EntityManager.Heroes.Enemies.Find(e => e.IsValidTarget(E.Range) && e.HasBuffOfType(BuffType.Poison));
+
+            if (R.IsReady() && Combo["Rc"].Cast<CheckBox>().CurrentValue && !target.IsDead && target.IsValidTarget(R.Range))
+            {
+                R.Cast(target.Position);
             }
 
-            var useQ = Combo["Qc"].Cast<CheckBox>().CurrentValue &&
-                       Player.CanUseSpell(SpellSlot.Q) == SpellState.Ready;
-            var useW = Combo["Wc"].Cast<CheckBox>().CurrentValue &&
-                       Player.CanUseSpell(SpellSlot.W) == SpellState.Ready;
-            var useE = Combo["Ec"].Cast<CheckBox>().CurrentValue &&
-                       Player.CanUseSpell(SpellSlot.E) == SpellState.Ready;
-            var useR = Combo["Rc"].Cast<CheckBox>().CurrentValue &&
-                       Player.CanUseSpell(SpellSlot.R) == SpellState.Ready;
-
-            if (enemies.Count() > 1)
-            {
-                if (useW)
-                {
-                    var aoePrediction = Prediction.Position.PredictCircularMissileAoe(enemies.Cast<Obj_AI_Base>().ToArray(), W.Range, W.Radius, W.CastDelay, W.Speed).OrderByDescending(r => r.GetCollisionObjects<Obj_AI_Minion>().Length).FirstOrDefault();
-                    if (aoePrediction != null)
-                    {
-                        var predictedHeroes = aoePrediction.GetCollisionObjects<AIHeroClient>();
-
-                        if (predictedHeroes.Length > 1 && (float)predictedHeroes.Length / enemies.Count() >= aoeratio)
-                        {
-                            W.Cast(aoePrediction.CastPosition);
-                            return;
-                        }
-                    }
-                }
-
-                if (useE)
-                {
-                    foreach (var target in allTargets)
-                    {
-                        if (!target.IsInRange(_Player, E.Range) && !E.IsReady())
-                            return;
-                        {
-                            if (E.IsReady() && useE)
-                            {
-
-                                E.Cast(target);
-                                Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
-
-                            }
-                            if (E.IsReady() && !useE)
-                            {
-                                E.Cast(target);
-                            }
-                        }
-                    }
-                }
+            if (R.IsReady() && Combo["Rc"].Cast<CheckBox>().CurrentValue &&  !target.IsDead && target.IsValidTarget(R.Range))
+            { 
+                R.Cast(target.Position);
             }
-            if (useW)
-            {
-                foreach (var target in allTargets)
-                {
-                    if (!W.IsReady() && _Player.Distance(target) >= 700) return;
-                    {
 
-                        var Wpred = W.GetPrediction(target);
-                        if (Wpred.HitChance >= HitChance.High && target.IsValidTarget(W.Range))
-                        {
-                            if (useW)
-                            {
-                                var Enemys = EntityManager.Heroes.Enemies.Where(x => x.IsInRange(_Player.Position, W.Range));
-                                if (Enemys != null)
-                                {
-                                    if (Enemys.Count() >= Combo["minWw"].Cast<Slider>().CurrentValue)
-                                    {
-                                        W.Cast(target.ServerPosition);
-                                    }
-                                    else if (Enemys.Count() >= Combo["minWw"].Cast<Slider>().CurrentValue)
-                                    {
-                                        W.Cast(target);
-                                    }
-                                }
-                            }
-                        }
-                    }
+            if (Combo["W"].Cast<CheckBox>().CurrentValue && W.IsReady() && target.IsValidTarget(W.Range))
+            {
+                var prediction = W.GetPrediction(target);
+                if (prediction.HitChance >= HitChance.High)
+                {
+                    W.Cast(prediction.CastPosition);
                 }
             }
 
-            if (useQ)
+            if (Combo["Q"].Cast<CheckBox>().CurrentValue && Q.IsReady() && target.IsValidTarget(Q.Range + 100))
             {
-                foreach (var target in allTargets)
+                var prediction = Q.GetPrediction(target);
+                if (prediction.HitChance >= HitChance.High)
                 {
-                    if (Q.IsReady())
-                    {
-                        var canHitMoreThanOneTarget =
-                          EntityManager.Heroes.Enemies.OrderByDescending(x => x.CountEnemyChampionsInRange(Q.Width))
-                          .FirstOrDefault(x => x.IsValidTarget(Q.Range) && x.CountEnemyChampionsInRange(Q.Width) >= 2);
-                        if (canHitMoreThanOneTarget != null)
-                        {
-                            var getAllTargets = EntityManager.Heroes.Enemies.FindAll(x => x.IsValidTarget() && x.IsInRange(canHitMoreThanOneTarget, Q.Width));
-                            var center = getAllTargets.Aggregate(Vector3.Zero, (current, x) => current + x.ServerPosition) / getAllTargets.Count;
-                            if (!center.IsZero)
-                            {
-                                var Qpred = Q.GetPrediction(target);
-                                if (Qpred.HitChance >= HitChance.High && target.IsValidTarget(Q.Range))
-                                {
-                                    Q.Cast(target);
-                                }
-                            }
-                        }
-                    }
+                    Q.Cast(prediction.CastPosition);
                 }
 
-                if (useQ)
-                {
-                    foreach (var target in allTargets)
-                    {
-                        var canHitMoreThanOneTarget =
-                          EntityManager.Heroes.Enemies.OrderByDescending(x => x.CountEnemyChampionsInRange(Q.Width))
-                          .FirstOrDefault(x => x.IsValidTarget(Q.Range) && x.CountEnemyChampionsInRange(Q.Width) >= 1);
-                        if (canHitMoreThanOneTarget != null)
-                        {
-                            var getAllTargets = EntityManager.Heroes.Enemies.FindAll(x => x.IsValidTarget() && x.IsInRange(canHitMoreThanOneTarget, Q.Width));
-                            var center = getAllTargets.Aggregate(Vector3.Zero, (current, x) => current + x.ServerPosition) / getAllTargets.Count;
-                            if (!center.IsZero)
-                            {
-                                var Qpred = Q.GetPrediction(target);
-                                if (Qpred.HitChance >= HitChance.High && target.IsValidTarget(Q.Range))
-                                {
-                                    Q.Cast(target);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (useQ)
-                {
-                    foreach (var target in allTargets)
-                    {
-                        var Qpred = Q.GetPrediction(target);
-                        if (Qpred.HitChance >= HitChance.High && target.IsValidTarget(Q.Range))
-                        {
-                            Core.DelayAction(() => Q.Cast(target), 100);
-                        }
-                    }
-                }
             }
 
-            if (useR)
-            {
-                foreach (var target in allTargets)
-                {
-                    var Enemys = EntityManager.Heroes.Enemies.Where(x => x.IsInRange(_Player.Position, R.Range - 25));
-                    if (Enemys != null)
-                    {
-                        if (Enemys.Count() >= Combo["Re"].Cast<Slider>().CurrentValue && target.IsFacing(_Player))
-                        {
-                            Player.IssueOrder(GameObjectOrder.MoveTo, target);
-                            R.Cast(target);
-                        }
-                        if (Enemys.Count() >= Combo["Re"].Cast<Slider>().CurrentValue)
-                        {
-                            Player.IssueOrder(GameObjectOrder.MoveTo, target);
-                            R.Cast(target);
-                        }
+            if (E.IsReady() && Combo["Ec"].Cast<CheckBox>().CurrentValue && PositionEnemys.IsValidTarget(E.Range) && (Q.IsOnCooldown || !target.IsInRange(Cassio, Q.Range)))
+            {             
+                E.Cast(PositionEnemys);
+            }
 
-                        if (target.IsFacing(_Player) && target.IsInRange(_Player, R.Range))
-                        {
-                            Player.IssueOrder(GameObjectOrder.MoveTo, target);
-                            R.Cast(target.ServerPosition);
-                        }
-                    }
-                    if (target.IsInRange(_Player, R.Range))
-                    {
-                        Player.IssueOrder(GameObjectOrder.MoveTo, target);
-                        R.Cast(target.ServerPosition);
-                    }
-                }
+            else if (E.IsReady() && Combo["Ec"].Cast<CheckBox>().CurrentValue && target.IsValidTarget(E.Range) && (Q.IsOnCooldown || !target.IsInRange(Cassio, Q.Range)))
+            {
+               E.Cast(target);
             }
         }
 
         private static void ByLane()
         {
-            var useQ = Farm["Qf"].Cast<CheckBox>().CurrentValue && Player.Instance.ManaPercent >= Farm["Manal"].Cast<Slider>().CurrentValue && Player.CanUseSpell(SpellSlot.Q) == SpellState.Ready;
-            var useW = Farm["Wf"].Cast<CheckBox>().CurrentValue && Player.Instance.ManaPercent >= Farm["Manal"].Cast<Slider>().CurrentValue && Player.CanUseSpell(SpellSlot.W) == SpellState.Ready;
-            var useE = Farm["Ef"].Cast<CheckBox>().CurrentValue && Player.Instance.ManaPercent >= Farm["Manal"].Cast<Slider>().CurrentValue && Player.CanUseSpell(SpellSlot.E) == SpellState.Ready;
+            var minion1 = EntityManager.MinionsAndMonsters.GetLaneMinions().Where(m => m.IsValidTarget(E.Range)).OrderBy(m => !(m.Health <= Logics.GetMinionTarget (m, SpellSlot.E))).ThenBy(m => !m.HasBuffOfType(BuffType.Poison)).ThenBy(m => m.Health).FirstOrDefault();
+            var minion2 = EntityManager.MinionsAndMonsters.GetLaneMinions().FirstOrDefault(m => m.IsValidTarget(E.Range) && m.HasBuffOfType(BuffType.Poison));
+            var minion3 = EntityManager.MinionsAndMonsters.GetLaneMinions().Where(m => m.IsValidTarget(W.Range)).ToArray();
+            if (minion3.Length == 0) return;
 
-            if (useQ)
+            var minionlocation = Prediction.Position.PredictCircularMissileAoe(minion3, W.Range, W.Width, W.CastDelay, W.Speed).OrderByDescending(r => r.GetCollisionObjects<Obj_AI_Minion>().Length).FirstOrDefault();
+
+            if (Farm["Qf"].Cast<CheckBox>().CurrentValue && Q.IsReady())
             {
-                var minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Player.Instance.Position, 850, false);
-                var predict = Prediction.Position.PredictCircularMissileAoe(minions.Cast<Obj_AI_Base>().ToArray(), Q.Range, Q.Radius, Q.CastDelay, Q.Speed).OrderByDescending(r => r.GetCollisionObjects<Obj_AI_Minion>().Length).FirstOrDefault();
-
-                if (predict != null && predict.CollisionObjects.Length >= Farm["Qq"].Cast<Slider>().CurrentValue)
+                var predictedMinion = minionlocation.GetCollisionObjects<Obj_AI_Minion>();
+                if (predictedMinion.Length >= Farm["Qq"].Cast<Slider>().CurrentValue)
                 {
-                    Q.Cast(predict.CastPosition);
-                    return;
-                }
-            }
-            if (useW)
-            {
-                var minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Player.Instance.Position, 800, false);
-                var predict = Prediction.Position.PredictCircularMissileAoe(minions.Cast<Obj_AI_Base>().ToArray(), W.Range, W.Radius, W.CastDelay, W.Speed).OrderByDescending(r => r.GetCollisionObjects<Obj_AI_Minion>().Length).FirstOrDefault();
-
-                if (predict != null && predict.CollisionObjects.Length >= Farm["Ww"].Cast<Slider>().CurrentValue)
-                {
-                    W.Cast(predict.CastPosition);
-                    return;
+                    Q.Cast(minionlocation.CastPosition);
                 }
             }
 
-            if (useE)
+            if (Farm["Q"].Cast<CheckBox>().CurrentValue && Q.IsReady() && _Player.ManaPercent >= Farm["Manal"].Cast<Slider>().CurrentValue)
             {
-                var minione = EntityManager.MinionsAndMonsters.GetLaneMinions().FirstOrDefault(m => m.IsValidTarget(E.Range) && m.HasBuffOfType(BuffType.Poison));
+                var predictedMinion = minionlocation.GetCollisionObjects<Obj_AI_Minion>();
+                if (predictedMinion.Length >= Farm["Qq"].Cast<Slider>().CurrentValue)
                 {
-                    E.Cast(minione);
+                    Q.Cast(minionlocation.CastPosition);
+                }
+            }
+
+            if (Farm["Wf"].Cast<CheckBox>().CurrentValue && W.IsReady() && _Player.ManaPercent >= Farm["Manal"].Cast<Slider>().CurrentValue)
+            {
+                var predictedMinion = minionlocation.GetCollisionObjects<Obj_AI_Minion>();
+                if (predictedMinion.Length >= Farm["Ww"].Cast<Slider>().CurrentValue)
+                {
+                    W.Cast(minionlocation.CastPosition);
+
+                }
+            }
+
+            if (Farm["Ef"].Cast<CheckBox>().CurrentValue && E.IsReady() && Cassio.ManaPercent >= Farm["Manal"].Cast<Slider>().CurrentValue && minion1.IsValidTarget(E.Range))
+            {
+                if (Farm["EOnly"].Cast<CheckBox>().CurrentValue)
+                {
+                    E.Cast(minion2);
                 }
             }
         }
@@ -403,20 +273,6 @@ namespace Cassiopeia_Beta_Fixed
                    unit.HasBuffOfType(BuffType.Knockup) || unit.HasBuffOfType(BuffType.Snare) ||
                    unit.HasBuffOfType(BuffType.Taunt) || unit.HasBuffOfType(BuffType.Suppression) ||
                    unit.HasBuffOfType(BuffType.Polymorph);
-        }
-        private static void DisableAAreset(AttackableUnit target, Orbwalker.PreAttackArgs args)
-        {
-            if (
-                !Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo) ||
-                target.Type != GameObjectType.AIHeroClient || !Combo["DisAA"].Cast<CheckBox>().CurrentValue)
-                return;
-
-            if (Prediction.Position.Collision.LinearMissileCollision(
-                target as Obj_AI_Base, _Player.ServerPosition.To2D(), target.Position.To2D(), int.MaxValue,
-                (int)target.BoundingRadius, 0))
-            {
-                Orbwalker.DisableAttacking = true;
-            }
         }
 
         private static void OnInterrupter(Obj_AI_Base sender, Interrupter.InterruptableSpellEventArgs e)
