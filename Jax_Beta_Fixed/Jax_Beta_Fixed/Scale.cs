61f3b3b400a11aeb9287343877bff6dfa8bcf0a1
NoShurim <Cair139Cair139@gmail.com>
2017-08-09 23:58:04 -0300

    Jax Update 7.16

---
+++ Jax_Beta_Fixed/Jax_Beta_Fixed/Scale.cs
@@ -0,0 +1,17 @@
+﻿using ScalingTypes = Jax_Beta_Fixed.Managers.DamageManager.ScalingTypes;
+
+
+namespace Jax_Beta_Fixed.Bases
+{
+    class Scale
+    {
+        public readonly float[] scaling;
+        public readonly ScalingTypes scalingType;
+
+        public Scale(float[] scaling, ScalingTypes scalingType)
+        {
+            this.scaling = scaling;
+            this.scalingType = scalingType;
+        }
+    }
+}
