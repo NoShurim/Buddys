0dc9306dcdbc7c6e10c4e1a562d9c851a36fd03b
NoShurim <Cair139Cair139@gmail.com>
2017-08-11 22:29:48 -0300

    Mode Test

---
+++ Thresh_Beta_Fixed/Thresh_Beta_Fixed/MyHero.cs
@@ -0,0 +1,129 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using System;
+using System.Linq;
+using static Thresh_Beta_Fixed.Menus;
+using static Thresh_Beta_Fixed.Spells;
+
+namespace Thresh_Beta_Fixed
+{
+    class MyHero
+    {
+        private static AIHeroClient Thresh => Player.Instance;
+
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += On_Complete;
+        }
+
+        private static void On_Complete(EventArgs args)
+        {
+            if (Thresh.Hero != Champion.Thresh) return;
+
+            Menus.Execute();
+            Spells.Execute();
+
+            Gapcloser.OnGapcloser += AntiGapCloser;
+            Interrupter.OnInterruptableSpell += Interupt;
+            Game.OnTick += Game_Update;
+        }
+
+        private static void Game_Update(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                ByCombo();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                ByLane();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+            {
+                ByJungle();
+            }
+            AutoHara();
+            Shild();
+
+        }
+
+        private static void Shild()
+        {
+            if (W.IsReady())
+            {
+                foreach (var ally in EntityManager.Heroes.Allies.Where(x => x.IsValidTarget(W.Range) && !x.IsMe && !x.IsDead))
+                {
+                    W.Cast(ally);
+                }
+            }
+        }
+
+        private static void ByCombo()
+        {
+            var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+            var targete = TargetSelector.GetTarget(E.Range, DamageType.Magical);
+
+            if (Qc() && Q.IsReady() && target.IsValidTarget(Q.Range))
+            {
+                CastQ(target);
+            }
+            if (Ec() && E.IsReady() && target.IsValidTarget(E.Range))
+            {
+                CastE(targete);
+            }
+            if (Rc() && R.IsReady() && target.IsInRange(Thresh, R.Range))
+            {
+                R.Cast();
+            }
+        }
+                            
+        private static void ByLane()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ByJungle()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AutoHara()
+        {
+            var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+
+            if (Hq() && Q.IsReady() && target.IsValidTarget(Q.Range) && target.Health < 25)
+            {
+                var prediction = Q.GetPrediction(target);
+                if (prediction.HitChance >= HitChance.High)
+                {
+                    Q.Cast(target);
+                }
+            }
+        }
+        
+
+        private static void AntiGapCloser(AIHeroClient sender, Gapcloser.GapcloserEventArgs e)
+        {
+            if (Gap() && sender.IsEnemy && sender.IsValidTarget(E.Range) && e.End.Distance(Thresh) <= 250)
+            {
+                E.Cast(e.End);
+            }
+            if (Gap() && sender.IsEnemy && sender.IsValidTarget(Q.Range) && e.End.Distance(Thresh) <= 250)
+            {
+                Q.Cast(e.End);
+            }
+        }
+
+        private static void Interupt(Obj_AI_Base sender, Interrupter.InterruptableSpellEventArgs e)
+        {
+            if (!sender.IsEnemy) return;
+
+            if (e.DangerLevel == DangerLevel.High && Int() && Q.IsReady())
+            {
+                Q.Cast(sender.Position);
+            }
+        }
+    }
+}
---
+++ Thresh_Beta_Fixed/Thresh_Beta_Fixed/Spells.cs
@@ -0,0 +1,76 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using static Thresh_Beta_Fixed.Menus;
+using EloBuddy.SDK.Events;
+using System;
+using SharpDX;
+using System.Linq;
+
+namespace Thresh_Beta_Fixed
+{
+    internal class Spells
+    {
+        public static Spell.Skillshot Q;
+        public static Spell.Active Q2;
+        public static Spell.Skillshot E;
+        public static Spell.Skillshot W;
+        public static Spell.Active R;
+
+        private static AIHeroClient _Player
+        {
+            get
+            {
+                return ObjectManager.Player;
+            }
+        }
+
+        internal static void Execute()
+        {
+            Q = new Spell.Skillshot(spellSlot: SpellSlot.Q, spellRange: 1050, skillShotType: SkillShotType.Linear, castDelay: 250, spellSpeed: 1550, spellWidth: 60)
+            {
+                AllowedCollisionCount = 0
+            };
+            W = new Spell.Skillshot(spellSlot: SpellSlot.W, spellRange: 950, skillShotType: SkillShotType.Circular, castDelay: 250, spellSpeed: 1550, spellWidth: 60)
+            {
+                AllowedCollisionCount = 1
+            };
+            E = new Spell.Skillshot(spellSlot: SpellSlot.W, spellRange: 775, skillShotType: SkillShotType.Linear, castDelay: 250, spellSpeed: 1550, spellWidth: 60)
+            {
+                AllowedCollisionCount = int.MaxValue
+            };
+            Q2 = new Spell.Active(SpellSlot.Q);
+
+            R = new Spell.Active(SpellSlot.R, 450);
+        }
+
+        internal static void CastQ(Obj_AI_Base target)
+        {
+            var preq = Q.GetPrediction(target);
+            if (preq.HitChancePercent >= Pq())
+            {
+                Q.Cast();
+            }
+            else
+            {
+                Q2.Cast();
+            }
+
+            if (Wc() && W.IsReady())
+            {
+                foreach (var ally in EntityManager.Heroes.Allies.Where(x => x.IsValidTarget(W.Range)))
+                {
+                    W.Cast(ally);
+                }
+            }
+        }
+
+        internal static void CastE(Obj_AI_Base targete)
+        {
+            if (E.IsReady() && targete.IsInRange(_Player, E.Range))
+            {
+                E.Cast(targete.Position.Extend(_Player.Position, Vector3.Distance(targete.Position, _Player.Position) + 400).To3D());
+            }
+        }
+    }
+}
