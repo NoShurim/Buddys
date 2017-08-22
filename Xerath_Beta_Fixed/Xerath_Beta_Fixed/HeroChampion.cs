93bae1c79db8e1ffbe4f614cd6dcd8839ee6489b
NoShurim <Cair139Cair139@gmail.com>
2017-08-22 20:27:58 -0300

    Fixed!

---
+++ Xerath_Beta_Fixed/Xerath_Beta_Fixed/HeroChampion.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using static Xerath_Beta_Fixed.SpellManager;
+using EloBuddy.SDK.Rendering;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using System.Collections.Generic;
+using System.Linq;
+using SharpDX;
+
+namespace Xerath_Beta_Fixed
+{
+    class HeroChampion
+    {
+        private static AIHeroClient Xerath => Player.Instance;
+
+        public static bool CastCheckbox(Menu obj, string value)
+        {
+            return obj[value].Cast<CheckBox>().CurrentValue;
+        }
+
+        public static bool CastKey(Menu obj, string value)
+        {
+            return obj[value].Cast<KeyBind>().CurrentValue;
+        }
+
+        public static int CastSlider(Menu obj, string value)
+        {
+            return obj[value].Cast<Slider>().CurrentValue;
+        }
+
+        public static float lastR;
+
+        public static Menu Xe, Combo, Harass, Lane, Jungle, Utimate, Misc, Draws;
+        public static readonly Dictionary<int, float> Timers = new Dictionary<int, float>();
+        static float BigQ;
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnComplete;
+        }
+
+        private static void OnComplete(EventArgs args)
+        {
+            if (Xerath.Hero != Champion.Xerath) return;
+            Chat.Print("[Addon] [Champion] [Xerath]", System.Drawing.Color.Blue);
+
+            SpellManager.Initialize();
+            Drawing.OnDraw += DrawsOnDraws;
+            Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpellCast;
+            Obj_AI_Base.OnNewPath += Obj_AI_Base_OnNewPath;
+            Game.OnTick += Game_OnUpdate;
+
+            Xe = MainMenu.AddMenu("Xerath", "Xerath");
+            Combo = Xe.AddSubMenu("Combo");
+            Combo.Add("Qc", new CheckBox("Use [Q]"));
+            Combo.Add("Qcc", new CheckBox("Use [Q] IsCharnell", false));
+            Combo.Add("Wc", new CheckBox("Use [W]"));
+            Combo.Add("Wcc", new CheckBox("Use [W] IsCharnell", false));
+            Combo.Add("Ec", new CheckBox("Use [E]"));
+            Combo.Add("Ecc", new CheckBox("Use [E] IsCharnell", false));
+            Combo.Add("Rc", new CheckBox("Use [R] Combo", false));
+            Combo.AddSeparator();
+            Combo.AddLabel("Settings [Range]");
+            Combo.Add("rangeQ", new Slider("Max Range > %", 300, 0, 300));
+            Combo.Add("rangeQ2", new Slider("Min Range > %", 200, 0, 200));
+            //Harass
+            Harass = Xe.AddSubMenu("Harass");
+            Harass.Add("Hq", new CheckBox("Use [Q]"));
+            Harass.Add("Hw", new CheckBox("Use [W]"));
+            Harass.Add("He", new CheckBox("Use [E]"));
+            Harass.AddSeparator();
+            Harass.AddLabel("Percent Mana");
+            Harass.Add("mana", new Slider("Mana Percent > %", 75, 1));
+            //Lane
+            Lane = Xe.AddSubMenu("LaneClear");
+            Lane.Add("Ql", new CheckBox("Use [Q]"));
+            Lane.Add("Wl", new CheckBox("Use [W]"));
+            Lane.AddSeparator();
+            Lane.AddLabel("Mana Percent");
+            Lane.Add("manal", new Slider("Mana Percent > %", 50, 1));
+            Lane.AddSeparator();
+            Lane.AddLabel("Minions Percent");
+            Lane.Add("Min", new Slider("Minion Percent [Q] > %", 3, 1, 6));
+            Lane.Add("MinW", new Slider("Minion Percent [W] > %", 3, 1, 6));
+            //Jungle
+            Jungle = Xe.AddSubMenu("JungleClear");
+            Jungle.Add("Qj", new CheckBox("Use [Q]"));
+            Jungle.Add("Wj", new CheckBox("Use [W]"));
+            Jungle.AddSeparator();
+            Jungle.AddLabel("Mana Percent");
+            Jungle.Add("manaj", new Slider("Mana Percent > %", 50, 1));
+            //Utimate
+            Utimate = Xe.AddSubMenu("Utimate");
+            Utimate.AddLabel("Key [T]");
+            Utimate.Add("Key", new KeyBind("Shoot charge on press", false, KeyBind.BindTypes.HoldActive, 'T'));
+            //Misc
+            Misc = Xe.AddSubMenu("Misc");
+            Misc.Add("gape", new CheckBox("Use [E] GapClose"));
+            Misc.Add("inte", new CheckBox("Use [E] Interrupt"));
+            //Draws
+            Draws = Xe.AddSubMenu("Drawings");
+            Draws.Add("Dq", new CheckBox("Use [Q] Draw"));
+            Draws.Add("Dw", new CheckBox("Use [W] Draw"));
+            Draws.Add("De", new CheckBox("Use [E] Draw"));
+            Draws.Add("Dr", new CheckBox("Use [R] Draw"));
+
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                ByCombo();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                ByLane();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+            {
+                ByJungle();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                ByHarass();
+            }
+            if (Utimate["Key"].Cast<KeyBind>().CurrentValue)
+            {
+                CastR();
+            }
+        }
+
+        private static void ByCombo()
+        {
+            var rangeq = CastSlider(Combo, "rangeQ");
+            var useR = CastKey(Utimate, "Key");
+            if (!Q.IsCharging)
+            {
+                if (W.IsBackRange(Orbwalker.ActiveModes.Combo))
+                {
+                    if (W.CastOnBestTarget())
+                    {
+                        return;
+                    }
+                }
+
+                if (E.IsBackRange(Orbwalker.ActiveModes.Combo))
+                {
+                    var target = E.GetTarget();
+                    if (target != null && (target.GetStunDuration() == 0 || target.GetStunDuration() < (Xerath.ServerPosition.Distance(target.ServerPosition) / E.Speed + E.CastDelay / 1000f) * 1000))
+                    {
+                        if (E.Cast(target))
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+
+            if (Q.IsBackRange(Orbwalker.ActiveModes.Combo))
+            {
+                var target = TargetSelector.GetTarget(Q.MaximumRange, DamageType.Magical);
+                if (target != null)
+                {
+                    var prediction = Q.GetPrediction(target);
+                    if (prediction.HitChance >= Q.MinimumHitChance)
+                    {
+                        if (!Q.IsCharging)
+                        {
+                            Q.StartCharging();
+                            return;
+                        }
+                        if (Q.Range == Q.MaximumRange)
+                        {
+                            if (Q.Cast(target))
+                            {
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            if (Xerath.IsInRange(prediction.UnitPosition + rangeq * (prediction.UnitPosition - Xerath.ServerPosition).Normalized(), Q.Range))
+                            {
+                                if (Q.Cast(prediction.CastPosition))
+                                {
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (Q.IsCharging)
+            {
+                return;
+            }
+        }
+
+        private static void ByLane()
+        {
+            var mana = CastSlider(Lane, "manal");
+            var minios = CastSlider(Lane, "Min");
+            var miniosW = CastSlider(Lane, "MinW");
+
+            var minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Xerath.ServerPosition, Q.MaximumRange, false).ToArray();
+            if (minions.Length == 0)
+            {
+                return;
+            }
+
+            // Q is charging, ignore mana check
+            if (Q.IsBackRange(Orbwalker.ActiveModes.LaneClear) && Q.IsCharging)
+            {
+                // Check if we are on max range with the minions
+                if (minions.Max(m => m.Distance(Xerath, true)) < Q.RangeSquared)
+                {
+                    if (Q.Cast(Q.GetBestLinearCastPosition(minions).CastPosition))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            // Validate that Q is not charging
+            if (Q.IsCharging)
+            {
+                return;
+            }
+
+            // Check mana
+            if (mana > Xerath.ManaPercent)
+            {
+                return;
+            }
+
+            if (Q.IsBackRange(Orbwalker.ActiveModes.LaneClear))
+            {
+                if (minions.Length >= minios)
+                {
+                    // Check if we would hit enough minions
+                    if (Q.GetBestLinearCastPosition(minions).HitNumber >= minios)
+                    {
+                        // Start charging
+                        Q.StartCharging();
+                        return;
+                    }
+                }
+            }
+
+            if (W.IsBackRange(Orbwalker.ActiveModes.LaneClear))
+            {
+                if (minions.Length >= miniosW)
+                {
+                    var farmLocation = W.GetCircularFarmLocation(minions);
+                    if (farmLocation.HitNumber >= miniosW)
+                    {
+                        if (W.Cast(farmLocation.CastPosition))
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void ByJungle()
+        {
+            if (!Q.IsBackRange(Orbwalker.ActiveModes.JungleClear) && !W.IsBackRange(Orbwalker.ActiveModes.JungleClear) && !E.IsBackRange(Orbwalker.ActiveModes.JungleClear))
+            {
+                return;
+            }
+
+            var minions = EntityManager.MinionsAndMonsters.GetJungleMonsters(Xerath.ServerPosition, W.Range, false).ToArray();
+            if (minions.Length == 0)
+            {
+                return;
+            }
+
+            if (Q.IsBackRange(Orbwalker.ActiveModes.JungleClear))
+            {
+                var farmLocation = EntityManager.MinionsAndMonsters.GetLineFarmLocation(minions, Q.Width, (int)(Q.IsCharging ? Q.Range : Q.MaximumRange));
+                if (farmLocation.HitNumber > 0)
+                {
+                    if (!Q.IsCharging)
+                    {
+                        Q.StartCharging();
+                        return;
+                    }
+                    if (Q.Cast(farmLocation.CastPosition))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            if (Q.IsCharging)
+            {
+                return;
+            }
+
+            if (W.IsBackRange(Orbwalker.ActiveModes.JungleClear))
+            {
+                var farmLocation = W.GetCircularFarmLocation(minions);
+                if (farmLocation.HitNumber > 0)
+                {
+                    if (W.Cast(farmLocation.CastPosition))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            if (E.IsBackRange(Orbwalker.ActiveModes.JungleClear))
+            {
+                E.Cast(minions[0]);
+            }
+        }
+
+        private static void ByHarass()
+        {
+            var rangeq = CastSlider(Combo, "rangeQ");
+            var mana = CastSlider(Harass, "mana");
+
+            if (Q.IsBackRange(Orbwalker.ActiveModes.Harass) && Q.IsCharging)
+            {
+                var target = TargetSelector.GetTarget(Q.MaximumRange, DamageType.Magical);
+                if (target != null)
+                {
+                    var prediction = Q.GetPrediction(target);
+                    if (prediction.HitChance >= Q.MinimumHitChance)
+                    {
+                        if (Q.IsFullyCharged)
+                        {
+                            if (Q.Cast(target))
+                            {
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            if (Xerath.IsInRange(prediction.UnitPosition + rangeq * (prediction.UnitPosition - Xerath.ServerPosition).Normalized(), Q.Range))
+                            {
+                                if (Q.Cast(prediction.CastPosition))
+                                {
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Validate that Q is not charging
+            if (Q.IsCharging)
+            {
+                return;
+            }
+
+            // Check mana
+            if (mana > Xerath.ManaPercent)
+            {
+                return;
+            }
+
+            if (W.IsBackRange(Orbwalker.ActiveModes.Harass))
+            {
+                if (W.CastOnBestTarget())
+                {
+                    return;
+                }
+            }
+
+            if (E.IsBackRange(Orbwalker.ActiveModes.Harass))
+            {
+                if (E.CastOnBestTarget())
+                {
+                    return;
+                }
+            }
+
+            // Q chargeup
+            if (Q.IsBackRange(Orbwalker.ActiveModes.Harass) && !Q.IsCharging)
+            {
+                var target = TargetSelector.GetTarget(Q.MaximumRange, DamageType.Magical);
+                if (target != null)
+                {
+                    var prediction = Q.GetPrediction(target);
+                    if (prediction.HitChance >= Q.MinimumHitChance)
+                    {
+                        Q.StartCharging();
+                    }
+                }
+            }
+        }
+
+        private static void DrawsOnDraws(EventArgs args)
+        {
+            if (Draws["Dq"].Cast<CheckBox>().CurrentValue && SpellManager.Q.IsReady())
+            {
+                Drawing.DrawCircle(Player.Instance.Position, SpellManager.Q.MinimumRange, System.Drawing.Color.Teal);
+            }
+            if (Draws["Dq"].Cast<CheckBox>().CurrentValue && SpellManager.Q.IsReady())
+            {
+                Drawing.DrawCircle(Player.Instance.Position, SpellManager.Q.MaximumRange, System.Drawing.Color.Teal);
+            }
+
+            if (Draws["Dw"].Cast<CheckBox>().CurrentValue && SpellManager.W.IsReady())
+            {
+                SpellManager.W.DrawRange(System.Drawing.Color.Crimson);
+            }
+            if (Draws["De"].Cast<CheckBox>().CurrentValue && SpellManager.E.IsReady())
+            {
+                SpellManager.E.DrawRange(System.Drawing.Color.Crimson);
+            }
+            if (Draws["Dr"].Cast<CheckBox>().CurrentValue && SpellManager.R.IsReady())
+            {
+                SpellManager.R.DrawRange(System.Drawing.Color.LawnGreen);
+            }
+        }
+
+        private static void Obj_AI_Base_OnNewPath(Obj_AI_Base sender, GameObjectNewPathEventArgs args)
+        {
+            int id = sender.NetworkId;
+            if (!Timers.ContainsKey(id))
+                return;
+            Timers[id] = Game.Time;
+        }
+
+        private static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            if (sender.IsMe && args.Slot == SpellSlot.Q && Game.Time > BigQ + 0.2f)
+            {
+                BigQ = Game.Time;
+            }
+        }
+    }
+}
---
+++ Xerath_Beta_Fixed/Xerath_Beta_Fixed/SpellManager.cs
@@ -0,0 +1,248 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Menu.Values;
+using static Xerath_Beta_Fixed.HeroChampion;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xerath_Beta_Fixed
+{
+    public static class SpellManager
+    {
+
+        public static Spell.Chargeable Q { get; private set; }
+        public static Spell.Skillshot W { get; private set; }
+        public static Spell.Skillshot E { get; private set; }
+        public static Spell.Skillshot R { get; private set; }
+
+        public static Spell.SpellBase[] Spells { get; private set; }
+        public static Dictionary<SpellSlot, Color> ColorTranslation { get; private set; }
+
+        public static bool IsCastingUlt
+        {
+            get { return Player.Instance.Buffs.Any(b => b.Caster.IsMe && b.IsValid() && b.DisplayName == "XerathR"); }
+        }
+        public static int LastChargeTime { get; private set; }
+        public static Vector3 LastChargePosition { get; private set; }
+        public static int MaxCharges
+        {
+            get { return !R.IsLearned ? 3 : 2 + R.Level; }
+        }
+        public static int ChargesRemaining { get; private set; }
+
+        public static bool TapKeyPressed { get; private set; }
+
+        public static void Initialize()
+        {
+            Q = new Spell.Chargeable(SpellSlot.Q, 750, 1500, 1500, 500, int.MaxValue, 95);
+            W = new Spell.Skillshot(SpellSlot.W, 1100, SkillShotType.Circular, 250, int.MaxValue, 500);
+            E = new Spell.Skillshot(SpellSlot.E, 1050, SkillShotType.Linear, 250, 1400, 60);
+            R = new Spell.Skillshot(SpellSlot.R, 3520, SkillShotType.Circular, 500, int.MaxValue, 120);
+
+            Q.AllowedCollisionCount = int.MaxValue;
+            W.AllowedCollisionCount = int.MaxValue;
+            R.AllowedCollisionCount = int.MaxValue;
+
+            Game.OnTick += OnTick;
+            Obj_AI_Base.OnProcessSpellCast += OnProcessSpellCast;
+        }
+
+        private static float _previousLevel;
+
+        private static void OnTick(EventArgs args)
+        {
+            // Adjust R range
+            if (_previousLevel < R.Level)
+            {
+                R.Range = Convert.ToUInt32(2000 + 1200 * R.Level);
+                _previousLevel = R.Level;
+            }
+        }
+
+        private static void OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            if (sender.IsMe)
+            {
+                switch (args.SData.Name)
+                {
+                    // Ult activation
+                    case "XerathLocusOfPower2":
+                        LastChargePosition = Vector3.Zero;
+                        LastChargeTime = 0;
+                        ChargesRemaining = MaxCharges;
+                        TapKeyPressed = false;
+                        break;
+                    // Ult charge usage
+                    case "xerathlocuspulse":
+                        LastChargePosition = args.End;
+                        LastChargeTime = Environment.TickCount;
+                        ChargesRemaining--;
+                        TapKeyPressed = false;
+                        break;
+                }
+            }
+        }
+        public static bool IsEnabled(this Spell.SpellBase spell, Orbwalker.ActiveModes mode)
+        {
+            var useQ = CastCheckbox(Combo, "Qc");
+            var useW = CastCheckbox(Combo, "Wc");
+            var useE = CastCheckbox(Combo, "Ec");
+            var useR1 = CastCheckbox(Combo, "Rc");
+            var useR = CastKey(Utimate, "Key");
+            //Harass
+            var usehQ = CastCheckbox(Harass, "Hq");
+            var usehW = CastCheckbox(Harass, "Hw");
+            var usehE = CastCheckbox(Harass, "He");
+            //Lane
+            var uselQ = CastCheckbox(Lane, "Ql");
+            var uselW = CastCheckbox(Lane, "Wl");
+            //Jungle
+            var usejQ = CastCheckbox(Jungle, "Qj");
+            var usejW = CastCheckbox(Jungle, "Wj");
+
+
+            switch (mode)
+            {
+                case Orbwalker.ActiveModes.Combo:
+                    switch (spell.Slot)
+                    {
+                        case SpellSlot.Q:
+                            return useQ;
+                        case SpellSlot.W:
+                            return useW;
+                        case SpellSlot.E:
+                            return useE;
+                        case SpellSlot.R:
+                            return useR && useR1;
+                    }
+                    break;
+                case Orbwalker.ActiveModes.Harass:
+                    switch (spell.Slot)
+                    {
+                        case SpellSlot.Q:
+                            return usehQ;
+                        case SpellSlot.W:
+                            return usehW;
+                        case SpellSlot.E:
+                            return usehE;
+                    }
+                    break;
+                case Orbwalker.ActiveModes.LaneClear:
+                    switch (spell.Slot)
+                    {
+                        case SpellSlot.Q:
+                            return uselQ;
+                        case SpellSlot.W:
+                            return uselW;
+                    }
+                    break;
+                case Orbwalker.ActiveModes.JungleClear:
+                    switch (spell.Slot)
+                    {
+                        case SpellSlot.Q:
+                            return usejQ;
+                        case SpellSlot.W:
+                            return usejW;
+
+                    }
+                    break;
+            }
+
+            return false;
+        }
+
+        public static bool IsBackRange(this Spell.SpellBase spell, Orbwalker.ActiveModes mode)
+        {
+            return spell.IsEnabled(mode) && spell.IsReady();
+        }
+
+        public static AIHeroClient GetTarget(this Spell.SpellBase spell, params AIHeroClient[] excludeTargets)
+        {
+            var targets = EntityManager.Heroes.Enemies.Where(o => o.IsValidTarget() && !excludeTargets.Contains(o) && spell.IsInRange(o)).ToArray();
+            return TargetSelector.GetTarget(targets, DamageType.Magical);
+        }
+
+        public static bool CastOnBestTarget(this Spell.SpellBase spell)
+        {
+            var target = spell.GetTarget();
+            return target != null && spell.Cast(target);
+        }
+
+       public static bool CastR()
+        {
+            if (!R.IsReady() || Game.Time < lastR + 0.8f)
+                return false;
+            int range = 2200 + (R.Level * 1320);
+            var enemy = TargetSelector.GetTarget(range, DamageType.Magical);
+            if (!enemy.IsValidTarget())
+                return false;
+            int enemyid = enemy.NetworkId;
+            Vector2 enemypos = enemy.Position.To2D();
+            float enemyspeed = enemy.MoveSpeed;
+            Vector3[] path = enemy.Path;
+            int lenght = path.Length;
+            Vector3 predpos = Vector3.Zero;
+            if (lenght > 1)
+            {
+                float s_in_time = enemyspeed * (Game.Time - Timers[enemyid] + (Game.Ping * 0.001f));
+                float d = 0f;
+                for (int i = 0; i < lenght - 1; i++)
+                {
+                    Vector2 vi = path[i].To2D();
+                    Vector2 vi1 = path[i + 1].To2D();
+                    d += vi.Distance(vi1);
+                    if (d >= s_in_time)
+                    {
+                        float dd = enemypos.Distance(vi1);
+                        float ss = enemyspeed * 0.5f;
+                        if (dd >= ss)
+                        {
+                            predpos = (enemypos + ((vi1 - enemypos).Normalized() * ss)).To3D();
+                            break;
+                        }
+                        if (i + 1 == lenght - 1)
+                        {
+                            predpos = (enemypos + ((vi1 - enemypos).Normalized() * enemypos.Distance(vi1))).To3D();
+                            break;
+                        }
+                        for (int j = i + 1; j < lenght - 1; j++)
+                        {
+                            Vector2 vj = path[j].To2D();
+                            Vector2 vj1 = path[j + 1].To2D();
+                            ss -= dd;
+                            dd = vj.Distance(vj1);
+                            if (dd >= ss)
+                            {
+                                predpos = (vj + ((vj1 - vj).Normalized() * ss)).To3D();
+                                break;
+                            }
+                            if (j + 1 == lenght - 1)
+                            {
+                                predpos = (vj + ((vj1 - vj).Normalized() * dd)).To3D();
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    if (i + 1 == lenght - 1)
+                    {
+                        predpos = (vi + ((vi1 - vi).Normalized() * vi.Distance(vi1))).To3D();
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                predpos = enemy.Position;
+            }
+            if (predpos.IsZero || predpos.Distance(Player.Instance.Position) > range || (int)path.LastOrDefault().X != (int)enemy.Path.LastOrDefault().X)
+                return false;
+            Player.Instance.Spellbook.CastSpell(SpellSlot.R, predpos);
+            lastR = Game.Time;
+            return true;
+        }
+    }
+}
