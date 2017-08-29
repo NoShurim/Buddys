30b1dc7e37e17ac1b754a966cc87c76a68c1357f
NoShurim <Cair139Cair139@gmail.com>
2017-08-28 21:45:43 -0300

    Beta!! Teste

---
+++ Evelynn_Beta_Fixed/Evelynn_Beta_Fixed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EloBuddy.SDK.Events;
+using EloBuddy;
+
+namespace Evelynn_Beta_Fixed
+{
+    class Program
+    {
+        public static AIHeroClient Evely => Player.Instance;
+
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_LoadingBasic;
+        }
+
+        private static void Loading_LoadingBasic(EventArgs args)
+        {
+            if (Evely.Hero != Champion.Evelynn) return;
+        }
+    }
+}
